166cfe5c2821cc4824cd04c5e457f9b9f84ed7ae
hwalli92 <62026003+hwalli92@users.noreply.github.com>
2021-05-26 08:56:27 +0200

    handle status read from hoverboad and print on LCD screen

--- src/lcd_screen.py
+++ src/lcd_screen.py
@@ -24,13 +24,13 @@ class LCDScreen(threading.Thread):
         self.lcd.clear()
 
         while not self.shutdown_flag.isSet():
-            #print(self.getHBStatus())
+            status = self.getHBStatus()
+            print(status)
+            self.lcd.clear()
             self.lcd.printline(0, "HoverRun".center(COLS))
-            self.lcd.printline(
-                1, "Hoverbrd Batt: {}%".format(self.getHBStatus())
-            )
-            self.lcd.printline(2, "Speed: {}".format(self.getHBStatus()))
-            self.lcd.printline(3, "Steer: {}".format(self.getHBStatus()))
+            self.lcd.printline(1, "Hoverbrd Batt: {}%".format((status[7] / 42) * 100))
+            self.lcd.printline(2, "SPD: L: {} R: {}".format(status[1], status[3]))
+            self.lcd.printline(3, "Steer: {}".format(status[5]))
 
             time.sleep(2)
 
@@ -41,10 +41,13 @@ class LCDScreen(threading.Thread):
         self.lcd.clear()
 
     def getPiBattery(self):
-        return (self.pi_battery.get_power()/4000)*100
+        return (self.pi_battery.get_power() / 4000) * 100
 
     def getHBStatus(self):
-        #self.serial.write("status")
-        #status = self.serial.read()
+        self.serial.write("status")
+        status = self.serial.read()
+        print(status)
 
-        return 100
+        values = [int(value) for value in status.split() if value.isdigit()]
+        # values = [1, 1000, 2, 1000, 3, 60, 4, 38]
+        return values
